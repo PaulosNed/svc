929f531b58402866a57fa04bdfbfc34c1d206852
Paulos Dessie Nedaw <pauldessie@gmail.com>
2023-04-07 11:39:48 +0300

    added router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div></div>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <header className="w-full">
+          <NavBar />
+        </header>
+
+        <main className="flex-auto">
+          <Routes>
+            {/* <Route path="/" element={<Home />}/> */}
+          </Routes>
+        </main>
+
+        <footer></footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className='my-4 flex justify-between items-center'></div>
+  )
+}
+
+export default NavBar
